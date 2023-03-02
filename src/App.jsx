15a472ff62a9ce18765ab11a96068b556465c0fe
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-03-02 15:09:25 +0530

    Feat : Vans-Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,11 @@ import {
 	BrowserRouter,
 	Routes,
 	Route,
-	Link,
 } from "react-router-dom";
 import { About, Home, Vans } from "./pages";
 import { Footer, Header } from "./components";
-import "./server";
-import { useEffect } from "react";
 
 function App() {
-	useEffect(() => {
-		fetch("./api/vans")
-			.then((res) => res.json())
-			.then((data) => console.log(data));
-	}, []);
-
 	return (
 		<BrowserRouter>
 			<Header />
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../server";
 
 function Vans() {
-	return <div>Vans</div>;
+	const [vans, setVans] = useState([]);
+
+	// load data on page first render
+	useEffect(() => {
+		fetch("/api/vans")
+			.then((res) => res.json())
+			.then((data) => setVans(data.vans));
+	}, []);
+
+	console.log(vans);
+
+	const vansEl = vans.map((van) => {
+		return (
+			<div key={van.id} className="van-tile">
+				<img src={van.imageUrl} alt={van.name} />
+				<div className="van-info">
+					<h3 className="van-name">{van.name}</h3>
+					<div className="van-price">
+						<span className="price">
+							${van.price}
+						</span>{" "}
+						<br />
+						<span className="per-unit">
+							/Day
+						</span>
+					</div>
+				</div>
+
+				<p className="van-type">{van.type}</p>
+			</div>
+		);
+	});
+
+	return (
+		<section className="vans-page">
+			<h2>Explore our van options</h2>
+			<div className="vans-container">{vansEl}</div>
+		</section>
+	);
 }
 
 export default Vans;
